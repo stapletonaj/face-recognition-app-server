9034da898afac6d56c66f0f2aeba106fd06712dc
Andrew Stapleton <stapleton.aj@gmail.com>
2018-07-28 13:37:23 +0930

    port change

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post("/imageurl", (req, res) => {
 
 
 // start server
-app.listen(process.event.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`server for app is running on port ${process.env.PORT}`)
 })
 
