b174f71728549e2bd586b096f7dab58b56860c86
Andrew Stapleton <stapleton.aj@gmail.com>
2018-07-30 09:11:57 +0930

    adding dadbase connection

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const   express = require('express'),
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'Andy',
-        password: 'bums',
-        database: 'smart-brain'
+        host: 'process.env.DATABASE_URL',
+        ssl: true,
     }
 });
 
